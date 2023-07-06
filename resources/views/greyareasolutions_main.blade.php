1d59002fd348fbe5f2d090b685b9310c54d83a1e
MaxMJH <MaxHarrisMJH@gmail.com>
2023-07-06 06:27:29 +0100

    Added route to landing page as well as HTML and CSS for said landing page. Section of page is currently not responsive, and is missing other pages.

---
+++ resources/views/greyareasolutions_main.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Staging - Grey Area Solutions</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=ABeeZee">
+  </head>
+  <body>
+    <main>
+      <div class="landingpage">
+        <div class="content-wrapper">
+          <footer>
+            <img src="{{ asset('images/logo.png') }}" alt="Logo">
+            <nav class="navigation">
+              <a href="#">About</a>
+              <a href="#">Portfolio</a>
+              <a href="#">Blog</a>
+              <a href="#">Contact Us</a>
+            </nav>
+          </footer>
+          <div class="title">
+            <div class="titles">
+              <h1>Grey Area Solutions</h1>
+              <h2>Empowering Code, Fortifying Security</h2>
+            </div>
+            <img src="{{ asset('images/github.png') }}" alt="GitHub">
+          </div>
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
