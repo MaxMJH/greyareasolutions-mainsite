ec41dc051abfa5cf5475d9e7e1f402b2f51e16f1
MaxMJH <MaxHarrisMJH@gmail.com>
2023-07-08 03:33:40 +0100

    Added second section containiing company mission statement. Also fixed CSS issues on landing page, as some browsers did not show content.

--- resources/views/greyareasolutions_main.blade.php
+++ resources/views/greyareasolutions_main.blade.php
@@ -30,5 +30,22 @@
         </div>
       </div>
     </main>
+    <section class="about">
+      <h2>Our Mission</h2>
+      <p>
+        At Grey Area Solutions, we <em>empower</em> business and individuals with <em>cutting-edge</em>
+        programming and cyber security <em>solutions</em>. By fortifying security measures through code,
+        we protect against evolving threats and enable our clients to navigate the digital landscape with
+        confidence. Through collaboration, innovation, and a commitment to excellence, we deliver tailored
+        strategies that address immediate challenges and provide a foundation for long-term success.
+        Our trusted partnerships, education, and continous learning cultivate a culture of <em>empowerment</em>.
+      </p>
+      <p>
+        With integrity and professionalism, we exceed expectations, building relationships based on <em>trust</em>
+        and mutual <em>respect</em>. "Empowering Code, Fortifying Security" is our driving force, unlocking potential
+        and ensuring organisations <em>thrive</em> in the grey area. Together, we embrace challenges, find solutions,
+        and drive growth and <em>security</em> for our clients.
+      </p>
+    </section>
   </body>
 </html>
