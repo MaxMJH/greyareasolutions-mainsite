d85abe7b056f0b898887025f4e01868b04f967b7
MaxMJH <MaxHarrisMJH@gmail.com>
2023-07-11 02:42:38 +0100

    Added final section to main page that shows portfolio.

--- resources/views/greyareasolutions_main.blade.php
+++ resources/views/greyareasolutions_main.blade.php
@@ -47,5 +47,48 @@
         and drive growth and <em>security</em> for our clients.
       </p>
     </section>
+    <section class="portfolio">
+      <h2>Company Portfolio</h2>
+      <p>
+        Here are a small sample of projects created by our programmers. These projects range from, tools assisting with
+        penetration testing, to applications concerning smart cities. Due to the company's infancy, the projects below
+        are numerous, however, the number of projects will increase over the next few years.
+      </p>
+      <div class="projects">
+        <div class="card">
+          <img src="{{ asset('images/generalapp.jpeg') }}" alt="Code">
+          <p>
+            The HTMLScanner project is a simple Java program that searches HTML pages for specific tags as well as comments
+          </p>
+        </div>
+        <div class="card">
+          <img src="{{ asset('images/smartparkingsite.jpeg') }}" alt="Code">
+          <p>
+            The Smart Parking Website allows administrators to set and collect data pertaining to carparks around Nottingham
+          </p>
+        </div>
+        <div class="card">
+          <img src="{{ asset('images/generalmobileapp.jpeg') }}" alt="Code">
+          <p>
+            The Smart Parking Mobile Application allows users to plan their journey by selecting a carpark and seeing its
+            capacity around Nottingham
+          </p>
+        </div>
+        <div class="card">
+          <img src="{{ asset('images/generalapp.jpeg') }}" alt="Code">
+          <p>
+            The Smart Parking XML Scanner allows for the scraping of XML forms, typically used to scrape data pertaining to
+            Nottingham parking data
+          </p>
+        </div>
+        <div class="card">
+          <img src="{{ asset('images/smartparkingsite.jpeg') }}" alt="Code">
+          <p>
+            The Maths Education Mobile Application allows for children to practice their mathematical knowledge - aimed at
+            those in KS2
+          </p>
+        </div>
+      </div>
+    </section>
   </body>
 </html>
