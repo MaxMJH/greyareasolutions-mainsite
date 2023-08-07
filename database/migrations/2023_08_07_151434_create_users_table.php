0dff3ee82b6ae73c52dbb1c90426a47f3327672b
MaxMJH <MaxHarrisMJH@gmail.com>
2023-08-07 17:50:42 +0000

    Added users table migration - currently no data has been added to it for tests.

---
+++ database/migrations/2023_08_07_151434_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->string('email', 255)->unique()->nullable($value = false);
+            $table->string('password', 255)->nullable($value = false);
+            $table->string('firstname', 50)->nullable($value = false);
+            $table->string('lastname', 50)->nullable($value = false);
+            $table->enum('role', ['user', 'blogger', 'admin'])->default($default = 'user')->nullable($value = false);
+            $table->timestamp('last_login', $precision = 0)->nullable($value = true);
+            $table->unsignedTinyInteger('failed_attempts')->default($value = 0)->nullable($value = false);
+            $table->unsignedTinyInteger('is_locked')->default($value = 0)->nullable($value = false);
+            $table->timestamp('account_creation_date', $precision = 0)->useCurrent()->nullable($value = false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
