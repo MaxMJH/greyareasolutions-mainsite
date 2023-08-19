8e325d6d5917409210e1eb8290730091d87319a7
MaxMJH <MaxHarrisMJH@gmail.com>
2023-08-19 15:20:09 +0000

    Added '/accounts' page to header if user is logged in and has the role of admin.

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,3 +1,7 @@
+@php
+    use App\Enums\RoleEnum;
+@endphp
+
 <header>
   <img id="logo" src="{{ asset('images/logo.png') }}" alt="Logo">
   <img id="toggler" src="{{ asset('images/navigationToggler.png') }}" alt="Open Navigation">
@@ -5,6 +9,9 @@
     <a href="/#about">About</a>
     <a href="/#portfolio">Portfolio</a>
     <a href="/blogs">Blog</a>
+    @if (Auth::check() && Auth::user()->role === RoleEnum::Admin)
+      <a href="/accounts">Accounts</a>
+    @endif
     <a href="mailto:contactus@greyareasolutions.net">Contact Us</a>
   </nav>
   @if (Auth::check())
